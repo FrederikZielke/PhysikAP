b22eb099dc714858cf30b8ae70c7c0ae0558fefa
Lennart Völz <lennart.voelz@kluth-service.de>
2022-11-28 19:41:23 +0100

    tabelle fuer freddy

--- V207_Viskosimeter/vXXX/plot.py
+++ V207_Viskosimeter/vXXX/plot.py
@@ -10,12 +10,12 @@ rho = (4 * m)/(3 * np.pi * r ** 3)
 print(rho)
 
 
-#plt.subplot(1, 2, 1)
-#plt.plot(x, y, label='Kurve')
-#plt.xlabel(r'$\alpha \mathbin{/} \unit{\ohm}$')
-#plt.ylabel(r'$y \mathbin{/} \unit{\micro\joule}$')
-#plt.legend(loc='best')
+plt.rcParams['figure.figsize'] = (10, 8)
+plt.rcParams['font.size'] = 16
 
+x, y = np.genfromtxt('content/fallzeitSteigendeTemperatur.txt', unpack=True)
+
+plt.plot(x, y, 'k.', label="Daten")
 
 # in matplotlibrc leider (noch) nicht möglich
 plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
