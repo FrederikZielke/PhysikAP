fcab6b5577bef73383d0f2ad3b3c35f73153aa51
Lennart Völz <lennart.voelz@kluth-service.de>
2022-12-11 22:49:41 +0100

    hallo

--- V303_Lock-In-Verstaerker/curvefit.py
+++ V303_Lock-In-Verstaerker/curvefit.py
@@ -15,14 +15,14 @@ data1, covariance_matrix1 = curve_fit(f , phi1, U_out, p0=(5, 1, 0, 0))
 uncertainties1 = np.sqrt(np.diag(covariance_matrix1))
 x = np.linspace(0, 360, 1000)
 plt.plot(x, f(x, *data1), 'k-', linewidth = 1.5)
-plt.plot(phi1, U_out, 'x', color = '#FF00FF', label = r'Messdaten')
+plt.plot(phi1, U_out, 'x', color = 'red', label = r'Messdaten')
 
 plt.legend(loc="best")
 plt.xlabel(r'$\phi \,$[°]')
 plt.ylabel(r'$U \,[V]$')
 
 plt.savefig('build/U_out.pdf')
-plt.show()
+#plt.show()
 
 plt.clf()
 
@@ -33,16 +33,24 @@ data2, covariance_matrix2 = curve_fit(f , phi2, U_outnoise, p0=(5, 1, 0, 0))
 
 uncertainties2 = np.sqrt(np.diag(covariance_matrix2))
 x = np.linspace(0, 360, 1000)
-plt.plot(x, f(x, *data2), 'k-')
-plt.plot(phi2, U_outnoise, 'kx', label = r'Messdaten')
+plt.plot(x, f(x, *data2), 'k-', label = r'Curve-Fit')
+plt.plot(phi2, U_outnoise, 'rx', label = r'Messdaten')
+plt.plot(x, (400*np.sin(np.radians(x)))/(np.pi), label = r'Theoriekurve')
 
 plt.legend(loc="best")
 plt.xlabel(r'$\phi \,$[°]')
 plt.ylabel(r'$U \,[V]$')
 
 plt.savefig('build/U_outnoise.pdf')
-#plt.show()
+plt.show()
 
-print()
+print("U ohne Noise")
+print(*data1)
+print(*uncertainties1)
+print("U mit Noise")
+print(*data2)
+print(*uncertainties2)
+
+
 
 
