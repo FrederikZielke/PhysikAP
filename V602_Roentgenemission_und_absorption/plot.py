b61fa8372a302918e02c5b85e53d5b525e40826e
Lennart Völz <lennart.voelz@kluth-service.de>
2023-04-08 00:32:14 +0200

    paar dinge noch

--- V602_Roentgenemission_und_absorption/plot.py
+++ V602_Roentgenemission_und_absorption/plot.py
@@ -1,21 +1,85 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy.constants as sc
 
-x = np.linspace(0, 10, 1000)
-y = x ** np.sin(x)
 
-plt.subplot(1, 2, 1)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \mathbin{/} \unit{\ohm}$')
-plt.ylabel(r'$y \mathbin{/} \unit{\micro\joule}$')
-plt.legend(loc='best')
+E_abs = 8.9789 * 10**3 #Absorption Edge Copper
+E_a_theorie = 8.048 * 10**3
+E_b_theorie = 8.907 * 10**3
+TZ = 29 #Kupfer
+R = 13.6
+d = 2.014 * 10**(-10)
+n = 1
+t_b = np.radians(20.2)
+t_a = np.radians(22.5)
+th_b1 = np.radians(40.8/2)
+th_b2 = np.radians(39.9/2)
+th_a1 = np.radians(45.5/2)
+th_a2 = np.radians(44.5/2)
+#Absorption
+t_zn = np.radians(40.1/2)
+t_br = np.radians(26.4/2)
+t_zr = np.radians(20/2)
+t_sr = np.radians(22.1/2)
+t = [t_zn, t_br, t_zr, t_sr]
+#minimale Wellenlänge
+l_a = (2*d*np.sin(t_a))/(n)
+l_b = (2*d*np.sin(t_b))/(n)
+lh_a1 = (2*d*np.sin(th_a1))/(n)
+lh_a2 = (2*d*np.sin(th_a2))/(n)
+lh_b1 = (2*d*np.sin(th_b1))/(n)
+lh_b2 = (2*d*np.sin(th_b2))/(n)
+#Absorption Energie
+l = [0, 0, 0, 0]
+E = []
+Kom = ["Zn: ", "Br: ", "Zr: ", "Sr: "]
+for i in range (len(l)):
+    l[i] = (2*d*np.sin(t[i]))/(n)
+    E.append((sc.h*sc.c)/(l[i])*6.242*10**(18))
+    print(Kom[i], E[i])
+#Absorption Abschirmung
+sig = []
+Z = [30, 35, 40, 38]
+print("Abschirmung")
+for i in range(len(E)):
+    sig.append(Z[i] - np.sqrt(E[i]/R))
+    print(Kom[i], sig[i])
+#Energie
+E_a = (sc.h*sc.c)/(l_a)
+E_b = (sc.h*sc.c)/(l_b)
+Eh_a1 = (sc.h*sc.c)/(lh_a1)
+Eh_a2 = (sc.h*sc.c)/(lh_a2)
+Eh_b1 = (sc.h*sc.c)/(lh_b1)
+Eh_b2 = (sc.h*sc.c)/(lh_b2)
+#Umrechnung in eV
+E_a = E_a * 6.242*10**(18)
+E_b = E_b * 6.242*10**(18)
+Eh_a1 = Eh_a1 * 6.242*10**(18)
+Eh_a2 = Eh_a2 * 6.242*10**(18)
+Eh_b1 = Eh_b1 * 6.242*10**(18)
+Eh_b2 = Eh_b2 * 6.242*10**(18)
 
-plt.subplot(1, 2, 2)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \mathbin{/} \unit{\ohm}$')
-plt.ylabel(r'$y \mathbin{/} \unit{\micro\joule}$')
-plt.legend(loc='best')
+#Differenz E-Halbwertszeit
+Eh_a = Eh_a2 - Eh_a1
+Eh_b = Eh_b2 - Eh_b1
 
-# in matplotlibrc leider (noch) nicht möglich
-plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-plt.savefig('build/plot.pdf')
+#Auflösungsvermögen
+A_a = E_a / Eh_a
+A_b = E_b / Eh_b
+
+#Abschirmkonstanten
+Tsig1 = TZ - np.sqrt(E_abs/R)
+Tsig2 = TZ - 2 * np.sqrt((E_abs/R) - (E_a_theorie/R))
+Tsig3 = TZ - 3 * np.sqrt((E_abs/R) - (E_b_theorie/R))
+
+print("Wellenlänge K_a: ", l_a)
+print("Wellenlänge K_b: ", l_b)
+print("Energie K_a: ", E_a)
+print("Energie K_b: ", E_b)
+print("Energie Halbwertsbreite K_a: ", Eh_a)
+print("Energie Halbwertsbreite K_b: ", Eh_b)
+print("Auflösungsv. K_a: ", A_a)
+print("Auflösungsv. K_b: ", A_b)
+print("Sigma 1: ", Tsig1)
+print("Sigma 2: ", Tsig2)
+print("Sigma 3: ", Tsig3)
---
+++ V602_Roentgenemission_und_absorption/rydberg.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.optimize import curve_fit
+
+E = np.array([8978.82, 13480.57, 17727.23, 16060.79])
+Z = np.array([30, 35, 40, 38])
+
+def f(x, a, b):
+    return a*x + b
+
+params, pcov = curve_fit(f, Z, np.sqrt(E))
+uncertainties = np.sqrt(np.diag(pcov))
+x = np.linspace(np.min(Z), np.max(Z), 100)
+plt.plot(Z, np.sqrt(E), "bx", label="Absorptionsenergie")
+plt.plot(x,f(x,*params),'k-', label='Ausgleichsgerade')
+plt.ylabel(r'$\sqrt{E\,[eV]}$')
+plt.xlabel(r'$Z$')
+plt.legend()
+plt.show()
+print(params[0], params[1])
