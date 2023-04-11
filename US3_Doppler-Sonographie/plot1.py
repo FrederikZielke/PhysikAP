8cf6620cc2266d2e77084d38dcd314a181191915
Lennart Völz <lennart.voelz@kluth-service.de>
2023-04-11 17:34:40 +0200

    Rechnung und Plots fertig

---
+++ US3_Doppler-Sonographie/plot1.py
@@ -0,0 +1,62 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy as sc
+
+rpm15, f_mean15, f_max15 = np.genfromtxt("content/messung15.txt", unpack = True)
+rpm30, f_mean30, f_max30 = np.genfromtxt("content/messung30.txt", unpack = True)
+rpm45, f_mean45, f_max45 = np.genfromtxt("content/messung45.txt", unpack = True)
+
+#Konstanten
+c_fl = 1800
+c_pr = 2700
+f_0 = 2*10**(6)
+#Winkel
+a = [np.radians(80.6), np.radians(70.5), np.radians(54.7)]
+
+#Arrays sind toll
+rpm = [rpm15, rpm30, rpm45]
+f_mean = [f_mean15, f_mean30, f_mean45]
+f_max = [f_max15, f_max30, f_max45]
+fdiff = [0, 0, 0]
+nice = ["15: ", "30: ", "45: "]
+
+#Frequenzverschiebung
+for i in range(len(f_mean)):
+    fdiff[i] = np.abs(f_mean[i] - f_max[i])
+    #print(nice[i] ,fdiff[i])
+
+v = [0, 0, 0]
+#Geschwindigkeit 
+for i in range(len(f_mean)):
+    v[i] = (fdiff[i] * c_fl)/(2*f_0*np.cos(a[i]))
+    #print(nice[i], v[i])
+
+plot_y = [0, 0, 0]
+for i in range(len(v)):
+    plot_y[i] = fdiff[i]/np.cos(a[i])
+    #print(nice[i], plot_y[i])
+
+plt.plot(v[0], plot_y[0], 'bx', label = 'Messwerte bei 15°')
+plt.xlabel(r'$v \,$[m/s]')
+plt.ylabel(r'$ \frac{\Delta f}{cos(\alpha)} \,$[Hz]')
+plt.grid()
+plt.legend()
+plt.savefig('build/15.pdf')
+plt.clf()
+
+plt.plot(v[1], plot_y[1], 'bx', label = 'Messwerte bei 30°')
+plt.xlabel(r'$v \,$[m/s]')
+plt.ylabel(r'$ \frac{\Delta f}{cos(\alpha)} \,$[Hz]')
+plt.grid()
+plt.legend()
+plt.savefig('build/30.pdf')
+plt.clf()
+
+plt.plot(v[2], plot_y[2], 'bx', label = 'Messwerte bei 45°')
+plt.xlabel(r'$v \,$[m/s]')
+plt.ylabel(r'$ \frac{\Delta f}{cos(\alpha)} \,$[Hz]')
+plt.grid()
+plt.legend()
+plt.savefig('build/45.pdf')
+
+
---
+++ US3_Doppler-Sonographie/plot2.py
@@ -0,0 +1,35 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy as sc
+
+#3870rpm
+s1, v1, I1 = np.genfromtxt("content/messung3870.txt", unpack = True)
+
+#6020rpm
+s2, v2, I2 = np.genfromtxt("content/messung6020.txt", unpack = True)
+
+#Umrechnung in SI
+s1 = s1*10**(-6)
+s2 = s2*10**(-6)
+v1 = v1/100
+v2 = v2/100
+
+#Plot s gegen v
+plt.plot(s1, v1, 'bx', label = 'Messwerte bei 3870rpm')
+plt.plot(s2, v2, 'rx', label = 'Messwerte bei 6020rpm')
+plt.ylabel(r'$v \,$[m/s]')
+plt.xlabel(r'$s$[m]')
+plt.grid()
+plt.legend()
+plt.savefig('build/s_gegen_v.pdf')
+plt.clf()
+
+#Plot s gegen I
+plt.plot(s1, I1, 'bx', label = 'Messwerte bei 3870rpm')
+plt.plot(s2, I2, 'rx', label = 'Messwerte bei 6020rpm')
+plt.ylabel(r'$I \,$[1000V^2/s]')
+plt.xlabel(r'$s$[m]')
+plt.grid()
+plt.legend()
+plt.savefig('build/s_gegen_I.pdf')
+plt.show()
