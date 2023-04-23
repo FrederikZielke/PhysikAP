45e0c1b7339c860c2e56a96b7ac4352eccbbda8c
Lennart Völz <lennart.voelz@kluth-service.de>
2023-04-23 16:12:59 +0200

    fertig

--- V602_Roentgenemission_und_absorption/plot.py
+++ V602_Roentgenemission_und_absorption/plot.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.constants as sc
-
+from uncertainties import unumpy as unp
+from uncertainties import ufloat
 
 E_abs = 8.9789 * 10**3 #Absorption Edge Copper
 E_a_theorie = 8.048 * 10**3
@@ -16,25 +17,43 @@ th_b1 = np.radians(40.8/2)
 th_b2 = np.radians(39.9/2)
 th_a1 = np.radians(45.5/2)
 th_a2 = np.radians(44.5/2)
+t_b_err = np.radians(0.1)
+t_a_err = np.radians(0.1)
+th_b1_err = np.radians(0.1)
+th_b2_err = np.radians(0.1)
+th_a1_err = np.radians(0.1)
+th_a2_err = np.radians(0.1)
+t_b_mitfehler = ufloat(t_b, t_b_err)
+t_a_mitfehler = ufloat(t_a, t_a_err)
+th_b1_mitfehler = ufloat(th_b1, th_b1_err)
+th_b2_mitfehler = ufloat(th_b2, th_b2_err)
+th_a1_mitfehler = ufloat(th_a1, th_a1_err)
+th_a2_mitfehler = ufloat(th_a2, th_a2_err)
 #Absorption
 t_zn = np.radians(40.1/2)
 t_br = np.radians(26.4/2)
 t_zr = np.radians(20/2)
 t_sr = np.radians(22.1/2)
+t_zn_err = np.radians(0.1)
+t_br_err = np.radians(0.1)
+t_zr_err = np.radians(0.1)
+t_sr_err = np.radians(0.1)
 t = [t_zn, t_br, t_zr, t_sr]
+t_err = [t_zn_err, t_br_err, t_zr_err, t_sr_err]
+t_mitfehler = unp.uarray(t, t_err)
 #minimale Wellenlänge
-l_a = (2*d*np.sin(t_a))/(n)
-l_b = (2*d*np.sin(t_b))/(n)
-lh_a1 = (2*d*np.sin(th_a1))/(n)
-lh_a2 = (2*d*np.sin(th_a2))/(n)
-lh_b1 = (2*d*np.sin(th_b1))/(n)
-lh_b2 = (2*d*np.sin(th_b2))/(n)
+l_a = (2*d*unp.sin(t_a_mitfehler))/(n)
+l_b = (2*d*unp.sin(t_b_mitfehler))/(n)
+lh_a1 = (2*d*unp.sin(th_b1_mitfehler))/(n)
+lh_a2 = (2*d*unp.sin(th_b2_mitfehler))/(n)
+lh_b1 = (2*d*unp.sin(th_a1_mitfehler))/(n)
+lh_b2 = (2*d*unp.sin(th_a2_mitfehler))/(n)
 #Absorption Energie
 l = [0, 0, 0, 0]
 E = []
 Kom = ["Zn: ", "Br: ", "Zr: ", "Sr: "]
 for i in range (len(l)):
-    l[i] = (2*d*np.sin(t[i]))/(n)
+    l[i] = (2*d*unp.sin(t_mitfehler[i]))/(n)
     E.append((sc.h*sc.c)/(l[i])*6.242*10**(18))
     print(Kom[i], E[i])
 #Absorption Abschirmung
@@ -42,7 +61,7 @@ sig = []
 Z = [30, 35, 40, 38]
 print("Abschirmung")
 for i in range(len(E)):
-    sig.append(Z[i] - np.sqrt(E[i]/R))
+    sig.append(Z[i] - unp.sqrt(E[i]/R))
     print(Kom[i], sig[i])
 #Energie
 E_a = (sc.h*sc.c)/(l_a)
@@ -68,9 +87,9 @@ A_a = E_a / Eh_a
 A_b = E_b / Eh_b
 
 #Abschirmkonstanten
-Tsig1 = TZ - np.sqrt(E_abs/R)
-Tsig2 = TZ - 2 * np.sqrt((E_abs/R) - (E_a_theorie/R))
-Tsig3 = TZ - 3 * np.sqrt((E_abs/R) - (E_b_theorie/R))
+Tsig1 = TZ - unp.sqrt(E_abs/R)
+Tsig2 = TZ - 2 * unp.sqrt((E_abs/R) - (E_a/R))
+Tsig3 = TZ - 3 * unp.sqrt((E_abs/R) - (E_b/R))
 
 print("Wellenlänge K_a: ", l_a)
 print("Wellenlänge K_b: ", l_b)
