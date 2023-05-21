1d94981f8df5978e5fa87ab08aefd6281dfdac0f
Freddy <frederik.zielke@tu-dortmund.de>
2023-05-21 18:30:33 +0200

    Auswertung fertig

--- US1_Ultraschall/geschwindigkeit.py
+++ US1_Ultraschall/geschwindigkeit.py
@@ -7,6 +7,7 @@ from scipy.stats import linregress
 # Daten einlesen
 n, t = np.genfromtxt('content/LaufzeitMessung.csv', delimiter=',', unpack=True)
 ax, bx, d = np.genfromtxt('content/SchieblehreMessung.csv', delimiter=',', unpack=True)
+c_lit = 2730
 
 # Daten umrechnen
 ax *= 1e-3
@@ -15,28 +16,34 @@ d *= 1e-3
 
 # t from microseconds to seconds
 t *= 1e-6
+t /= 2
 
 bx[9] = bx[10]
 
 
 # t gegen bx plotten
 plt.plot( t, bx[2:10], 'kx', label='Messwerte')
-plt.xlabel(r'$t \:/\: $[s]')
-plt.ylabel(r'$x \:/\: $[m]')
+plt.xlabel(r'Laufzeit $t \:/\: $[s]')
+plt.ylabel(r'Lochhöhe $x \:/\: $[m]')
 
 
 # lineare Regression
 params, covariance_matrix = np.polyfit(t, bx[2:10], deg=1, cov=True)
 errors = np.sqrt(np.diag(covariance_matrix))
 
+# Abweichung von Theorie berechnen
+Abweichung = np.abs((params[0] - c_lit)/c_lit)*100
+
 # Werte ausgeben
 print('Steigung: ', params[0], '±', errors[0])
 print('y-Achsenabschnitt: ', params[1], '±', errors[1])
+print(f'Die Abweichung des Experimentell ermittelten Wertes beträgt {Abweichung} %')
 
 # Regression plotten
 x_plot = np.linspace(0, max(t))
 plt.plot(x_plot, params[0] * x_plot + params[1], 'r-', label='Lineare Regression')
 plt.legend(loc='best')
 plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+plt.xlim(0)
 plt.savefig('build/AcrylGeschwindigkeit.pdf')
 #plt.show()
--- US1_Ultraschall/lol.py
+++ US1_Ultraschall/lol.py
@@ -1,2 +1,49 @@
-for i  in range(1,12):
-    print(f'{i} & ')
+import numpy as np
+from uncertainties import unumpy as unp
+from uncertainties import ufloat
+
+# Messwerte einlesen aus A-Scan_Acryl.csv
+a, b, n = np.genfromtxt('content/A-scan_Acryl.csv', delimiter=',', unpack=True)
+
+h = ufloat(79.76, 0.04)
+ausgleich = ufloat(-3, 1.4)
+
+# UArrays
+af = unp.uarray(a, np.zeros(len(a)))
+bf = unp.uarray(b, np.zeros(len(b)))
+
+# Berechnung der Durchmesser
+d = h -af -bf -ausgleich
+print(d)
+g = a + b
+print(g)
+print('A-Scan:')
+
+for i in range(len(a)):
+    print(f'{n[i]} & {a[i]} & {b[i]} & {h  - af[i] - bf[i] - ausgleich}')
+
+for i in range(len(af)):
+    print(f'Durchmesser Loch {i+3} = {h - af[i] - bf[i]- ausgleich}')
+
+
+
+print('B-Scan:')
+
+# Messwerte einlesen aus dem B-Scan
+n, a, b , afalsch= np.genfromtxt('content/B-scan_Acryl.csv', delimiter=',', unpack=True)
+
+af = unp.uarray(a, np.zeros(len(a)))
+bf = unp.uarray(b, np.zeros(len(b)))
+afalschf = unp.uarray(afalsch, np.zeros(len(afalsch)))
+
+for i in range(len(af)):
+    print(f'Durchmesser Loch {i+3} = {h  - af[i] - bf[i] - ausgleich}')
+
+d = h -af -bf -ausgleich
+print(d)
+g = a + b
+print(g)
+for i in range(len(a)):
+    print(f'{n[i]} & {a[i]} & {b[i]} & {h  - af[i] - bf[i] - ausgleich}')
+
+#
